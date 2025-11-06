5a672b218358cce44d31a20b922d011c8f8650cd
kxw07 <kxw07@github.com>
2025-11-06 16:11:44 +0800

    feat: DynamicArray add base function

--- algorithm/src/main/java/others/DynamicArray.java
+++ algorithm/src/main/java/others/DynamicArray.java
@@ -1,19 +1,46 @@
 package others;
 
 public class DynamicArray {
-    private int dataIndex = -1;
+    private int itemIndex = 0;
     private Object[] array;
-    private int arraySize = 16;
+    private int itemSize = 0;
+    private int capacity = 16;
 
-    public DynamicArray(int size) {
-        while (this.arraySize <= size) {
-            this.arraySize = this.arraySize << 1;
+    public DynamicArray() {
+        this.array = new Object[this.capacity];
+    }
+
+    public DynamicArray(int expectCapacity) {
+        while (this.capacity < expectCapacity) {
+            this.capacity = this.capacity << 1;
         }
 
-        this.array = new Object[this.arraySize];
+        this.array = new Object[this.capacity];
     }
 
     public int size() {
-        return this.arraySize;
+        return this.itemSize;
+    }
+
+    public int capacity() {
+        return this.capacity;
+    }
+
+    public boolean isEmpty() {
+        return itemSize == 0;
+    }
+
+    public void push(Object item) {
+        this.array[this.itemIndex] = item;
+        this.itemIndex++;
+        this.itemSize++;
+    }
+
+    public Object at(int index) {
+        if (index < 0 || index > this.capacity) {
+            throw new RuntimeException("out of bounds");
+        }
+
+        return this.array[index];
     }
 }
--- algorithm/src/test/java/others/DynamicArrayTest.java
+++ algorithm/src/test/java/others/DynamicArrayTest.java
@@ -5,17 +5,81 @@ import org.junit.jupiter.api.Test;
 
 class DynamicArrayTest {
 
-    @Test
-    public void given_size_smaller_than_16_when_new_then_size_is_16() {
-        DynamicArray dynamicArray = new DynamicArray(8);
+    private DynamicArray dynamicArray;
 
-        Assertions.assertEquals(16, dynamicArray.size());
+    @Test
+    public void given_expectCapacity_smaller_than_16_when_calling_capacity_then_return_16() {
+        dynamicArray = new DynamicArray(8);
+
+        Assertions.assertEquals(16, dynamicArray.capacity());
     }
 
     @Test
-    public void given_size_greater_than_16_when_new_then_size_is_ceiling_of_2_power() {
-        DynamicArray dynamicArray = new DynamicArray(27);
+    public void given_expectCapacity_equal_16_when_calling_capacity_then_return_16() {
+        dynamicArray = new DynamicArray(16);
 
-        Assertions.assertEquals(32, dynamicArray.size());
+        Assertions.assertEquals(16, dynamicArray.capacity());
+    }
+
+    @Test
+    public void given_expectCapacity_greater_than_16_when_calling_capacity_then_return_ceiling_of_2_power() {
+        dynamicArray = new DynamicArray(27);
+
+        Assertions.assertEquals(32, dynamicArray.capacity());
+    }
+
+    @Test
+    public void given_push_two_objects_when_calling_size_then_return_2() {
+        dynamicArray = new DynamicArray();
+        dynamicArray.push(1);
+        dynamicArray.push(2);
+
+        Assertions.assertEquals(2, dynamicArray.size());
+    }
+
+    @Test
+    public void given_nothing_when_calling_size_then_return_0() {
+        dynamicArray = new DynamicArray();
+
+        Assertions.assertEquals(0, dynamicArray.size());
+    }
+
+    @Test
+    public void given_nothing_when_calling_isEmpty_then_true() {
+        dynamicArray = new DynamicArray();
+
+        Assertions.assertTrue(dynamicArray.isEmpty());
+    }
+
+    @Test
+    public void given_push_one_object_when_calling_isEmpty_then_false() {
+        dynamicArray = new DynamicArray();
+        dynamicArray.push(1);
+
+        Assertions.assertFalse(dynamicArray.isEmpty());
+    }
+
+    @Test
+    public void call_at_when_index_out_of_bounds_then_throw_runtime_exception() {
+        dynamicArray = new DynamicArray(16);
+
+        RuntimeException actual = Assertions.assertThrows(RuntimeException.class, () -> dynamicArray.at(999));
+        Assertions.assertEquals("out of bounds", actual.getMessage());
+    }
+
+    @Test
+    public void call_at_when_item_exists_then_return_item() {
+        dynamicArray = new DynamicArray();
+        Object expectItem = new Object();
+        dynamicArray.push(expectItem);
+
+        Assertions.assertEquals(expectItem, dynamicArray.at(0));
+    }
+
+    @Test
+    public void call_at_when_item_not_exists_then_return_null() {
+        dynamicArray = new DynamicArray(16);
+
+        Assertions.assertNull(dynamicArray.at(8));
     }
 }
