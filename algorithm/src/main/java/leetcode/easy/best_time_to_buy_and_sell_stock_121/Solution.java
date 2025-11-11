ce0a0762e9f94239ea1b641ccdf2c1d485d8816b
kxw07 <kxw07@github.com>
2025-11-11 22:37:42 +0800

    feat: leetcode 121 add Big-O comment

--- algorithm/src/main/java/leetcode/easy/best_time_to_buy_and_sell_stock_121/Solution.java
+++ algorithm/src/main/java/leetcode/easy/best_time_to_buy_and_sell_stock_121/Solution.java
@@ -9,12 +9,15 @@ public class Solution {
         int maxProfit = 0;
         int minPrice = prices[0];
 
+        // O(N)
         for(int i = 1; i < prices.length; i++) {
 
+            // O(1)
             if (prices[i] < minPrice) {
                 minPrice = prices[i];
             }
 
+            // O(1)
             if ((prices[i] - minPrice) > maxProfit) {
                 maxProfit = prices[i] - minPrice;
             }
