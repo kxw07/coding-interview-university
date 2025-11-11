898c5ef52e3fd4fd38818d3e0b1d03942518923a
kxw07 <kxw07@github.com>
2025-11-11 11:41:25 +0800

    feat: leetcode 26 dummy answer

--- algorithm/src/main/java/leetcode/grind75/remove_duplicates_from_sorted_array_26/Solution.java
+++ algorithm/src/main/java/leetcode/grind75/remove_duplicates_from_sorted_array_26/Solution.java
@@ -2,6 +2,37 @@ package leetcode.grind75.remove_duplicates_from_sorted_array_26;
 
 public class Solution {
     public int removeDuplicates(int[] nums) {
+        int unduplicatedElements = 0;
+        int lastElement = -999;
 
+        // total: O(N)
+        // O(N)
+        for (int idx = 0; idx < nums.length; idx++) {
+            // O(1)
+            if (nums[idx] == lastElement) {
+                nums[idx] = 999;
+            } else {
+                lastElement = nums[idx];
+                unduplicatedElements++;
+            }
+        }
+
+        // total: O(N^2)
+        // O(N)
+        for (int outsideIdx = 0; outsideIdx < nums.length; outsideIdx++) {
+            // O(N)
+            for (int insideIdx = outsideIdx + 1; insideIdx < nums.length; insideIdx++) {
+                // O(1)
+                if (nums[outsideIdx] > nums[insideIdx]) {
+                    int temp = nums[outsideIdx];
+                    nums[outsideIdx] = nums[insideIdx];
+                    nums[insideIdx] = temp;
+                }
+            }
+        }
+
+        // O(N) + O(N^2) = O(N^2)
+
+        return unduplicatedElements;
     }
 }
---
+++ algorithm/src/test/java/leetcode/grind75/remove_duplicates_from_sorted_array_26/SolutionTest.java
@@ -0,0 +1,36 @@
+package leetcode.grind75.remove_duplicates_from_sorted_array_26;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution sut;
+
+    @BeforeEach
+    public void setup() {
+        sut = new Solution();
+    }
+
+    @Test
+    public void case1() {
+        int[] nums = {1, 1, 2};
+        int expectedK = 2;
+        int[] expectedNums = {1, 2, 999};
+
+        Assertions.assertEquals(expectedK, sut.removeDuplicates(nums));
+        Assertions.assertArrayEquals(expectedNums, nums);
+    }
+
+    @Test
+    public void case2() {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int expectedK = 5;
+        int[] expectedNums = {0, 1, 2, 3, 4, 999, 999, 999, 999, 999};
+
+        Assertions.assertEquals(expectedK, sut.removeDuplicates(nums));
+        Assertions.assertArrayEquals(expectedNums, nums);
+    }
+
+}
