37235614c1b5e62d0719738c85e40029ae9670c6
kxw07 <kxw07@github.com>
2025-11-11 14:04:33 +0800

    feat: leetcode 88 dummy answer

--- algorithm/src/main/java/leetcode/easy/merge_sorted_array_88/Solution.java
+++ algorithm/src/main/java/leetcode/easy/merge_sorted_array_88/Solution.java
@@ -2,6 +2,38 @@ package leetcode.easy.merge_sorted_array_88;
 
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n == 0) {
+            return;
+        }
 
+        if(m == 0) {
+            // O(N)
+            for(int i=0; i<n; i++) {
+                // O(1)
+                nums1[i] = nums2[i];
+            }
+        }
+
+        // O(N)
+        for(int i=0; i<n; i++) {
+            // O(1)
+            nums1[m+i] = nums2[i];
+        }
+
+        int totalLength = m + n;
+        // O(N)
+        for(int outsideIndex = 0; outsideIndex < totalLength; outsideIndex++) {
+            // O(N)
+            for(int insideIndex = outsideIndex; insideIndex < totalLength; insideIndex++) {
+                // O(1)
+                if (nums1[outsideIndex] > nums1[insideIndex]) {
+                    int temp = nums1[outsideIndex];
+                    nums1[outsideIndex] = nums1[insideIndex];
+                    nums1[insideIndex] = temp;
+                }
+            }
+        }
+
+        // total: O(N) + O(N^2) = O(N^2)
     }
 }
---
+++ algorithm/src/test/java/leetcode/easy/merge_sorted_array_88/SolutionTest.java
@@ -0,0 +1,55 @@
+package leetcode.easy.merge_sorted_array_88;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new Solution();
+    }
+
+    @Test
+    public void case1() {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        int[] expected = {1, 2, 2, 3, 5, 6};
+
+        sut.merge(nums1, m, nums2, n);
+
+        Assertions.assertArrayEquals(expected, nums1);
+    }
+
+    @Test
+    public void case2() {
+        int[] nums1 = {1};
+        int m = 1;
+        int[] nums2 = {};
+        int n = 0;
+        int[] expected = {1};
+
+        sut.merge(nums1, m, nums2, n);
+
+        Assertions.assertArrayEquals(expected, nums1);
+    }
+
+    @Test
+    public void case3() {
+        int[] nums1 = {0};
+        int m = 0;
+        int[] nums2 = {1};
+        int n = 1;
+        int[] expected = {1};
+
+        sut.merge(nums1, m, nums2, n);
+
+        Assertions.assertArrayEquals(expected, nums1);
+    }
+
+}
