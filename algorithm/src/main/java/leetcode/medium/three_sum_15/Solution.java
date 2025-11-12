9c00ecd142306d7b154be0018f1d20e29d8aa595
kxw07 <kxw07@github.com>
2025-11-12 11:41:56 +0800

    feat: leetcode 15 first answer, O(N^3)

--- algorithm/src/main/java/leetcode/medium/three_sum_15/Solution.java
+++ algorithm/src/main/java/leetcode/medium/three_sum_15/Solution.java
@@ -1,9 +1,64 @@
 package leetcode.medium.three_sum_15;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3) {
+            return new ArrayList<>();
+        }
 
+        int firstNumberPointer = 0;
+        int secondaryNumberPointer = 1;
+        int thirdNumberPointer = 2;
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        while (firstNumberPointer < (nums.length - 2)) {
+            List<Integer> temp = new ArrayList<>();
+            int sum = nums[firstNumberPointer] + nums[secondaryNumberPointer] + nums[thirdNumberPointer];
+
+            if (sum == 0) {
+                temp.add(nums[firstNumberPointer]);
+                temp.add(nums[secondaryNumberPointer]);
+                temp.add(nums[thirdNumberPointer]);
+                result.add(temp);
+            }
+
+            thirdNumberPointer++;
+            if (thirdNumberPointer == nums.length) {
+                secondaryNumberPointer++;
+                thirdNumberPointer = secondaryNumberPointer + 1;
+
+                if (secondaryNumberPointer == nums.length - 1) {
+                    firstNumberPointer++;
+                    secondaryNumberPointer = firstNumberPointer + 1;
+                    thirdNumberPointer = secondaryNumberPointer + 1;
+                }
+            }
+        }
+
+        int max = result.size();
+        int index = 0;
+        while (max > 0 && index < result.size()) {
+            List<Integer> first = result.get(index);
+            first = first.stream().sorted().toList();
+
+            for (int i = index + 1; i < result.size(); i++) {
+                List<Integer> second = result.get(i);
+                second = second.stream().sorted().toList();
+
+                if (first.equals(second)) {
+                    result.remove(i);
+                    i--;
+                }
+            }
+
+            max--;
+            index++;
+        }
+
+        return result;
     }
 }
---
+++ algorithm/src/test/java/leetcode/medium/three_sum_15/SolutionTest.java
@@ -0,0 +1,121 @@
+package leetcode.medium.three_sum_15;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SolutionTest {
+
+    private Solution sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new Solution();
+    }
+
+    @Test
+    public void case1() {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> expect = List.of(List.of(-1, -1, 2), List.of(-1, 0, 1));
+
+        List<List<Integer>> actual = sut.threeSum(nums);
+        Assertions.assertEquals(expect.size(), actual.size());
+
+        boolean checkResult = false;
+        for (int i = 0; i < expect.size(); i++) {
+            List<Integer> e = expect.get(i).stream().sorted().toList();
+            checkResult = false;
+
+            for (int j = 0; j < actual.size(); j++) {
+                List<Integer> a = actual.get(j).stream().sorted().toList();
+
+                if (e.equals(a)) {
+                    checkResult = true;
+                }
+            }
+        }
+        Assertions.assertTrue(checkResult);
+    }
+
+    @Test
+    public void case2() {
+        int[] nums = {0, 1, 1};
+        List<List<Integer>> expect = new ArrayList<>();
+
+        List<List<Integer>> actual = sut.threeSum(nums);
+        Assertions.assertEquals(expect.size(), actual.size());
+
+        boolean checkResult = true;
+        for (int i = 0; i < expect.size(); i++) {
+            List<Integer> e = expect.get(i).stream().sorted().toList();
+            checkResult = false;
+
+            for (int j = 0; j < actual.size(); j++) {
+                List<Integer> a = actual.get(j).stream().sorted().toList();
+
+                if (e.equals(a)) {
+                    checkResult = true;
+                }
+            }
+        }
+        Assertions.assertTrue(checkResult);
+    }
+
+    @Test
+    public void case3() {
+        int[] nums = {0, 0, 0};
+        List<List<Integer>> expect = List.of(List.of(0, 0, 0));
+
+        List<List<Integer>> actual = sut.threeSum(nums);
+        Assertions.assertEquals(expect.size(), actual.size());
+
+        boolean checkResult = true;
+        for (int i = 0; i < expect.size(); i++) {
+            List<Integer> e = expect.get(i).stream().sorted().toList();
+            checkResult = false;
+
+            for (int j = 0; j < actual.size(); j++) {
+                List<Integer> a = actual.get(j).stream().sorted().toList();
+
+                if (e.equals(a)) {
+                    checkResult = true;
+                }
+            }
+        }
+        Assertions.assertTrue(checkResult);
+    }
+
+    @Test
+    public void case4() {
+        int[] nums = {2, -3, 0, -2, -5, -5, -4, 1, 2, -2, 2, 0, 2, -4, 5, 5, -10};
+        List<List<Integer>> expect = List.of(
+                List.of(-10, 5, 5),
+                List.of(-5, 0, 5),
+                List.of(-4, 2, 2),
+                List.of(-3, -2, 5),
+                List.of(-3, 1, 2),
+                List.of(-2, 0, 2));
+
+        List<List<Integer>> actual = sut.threeSum(nums);
+        System.out.println("actual: " + actual);
+        Assertions.assertEquals(expect.size(), actual.size());
+
+        boolean checkResult = true;
+        for (int i = 0; i < expect.size(); i++) {
+            List<Integer> e = expect.get(i).stream().sorted().toList();
+            checkResult = false;
+
+            for (int j = 0; j < actual.size(); j++) {
+                List<Integer> a = actual.get(j).stream().sorted().toList();
+
+                if (e.equals(a)) {
+                    checkResult = true;
+                }
+            }
+        }
+        Assertions.assertTrue(checkResult);
+    }
+}
