c244e86ca604cb754eaf70c0246a01bca2a814f7
kxw07 <kxw07@github.com>
2025-11-12 13:17:45 +0800

    feat: leetcode 15 improve two pointer version

--- algorithm/src/main/java/leetcode/medium/three_sum_15/Solution.java
+++ algorithm/src/main/java/leetcode/medium/three_sum_15/Solution.java
@@ -4,11 +4,13 @@ import java.util.*;
 
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         Set<List<Integer>> result = new HashSet<>();
 
-        nums = Arrays.stream(nums).sorted().toArray();
-
         for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] > 0) break;
+
             int head = i + 1;
             int tail = nums.length - 1;
 
@@ -19,6 +21,13 @@ public class Solution {
                     List<Integer> triplet = Arrays.asList(nums[i], nums[head], nums[tail]);
                     result.add(triplet);
                     head++;
+                    tail--;
+                    while (head < tail && nums[head] == nums[head - 1]) {
+                        head++;
+                    }
+                    while (head < tail && nums[tail] == nums[tail + 1]) {
+                        tail--;
+                    }
                 } else if (sum > 0) {
                     tail--;
                 } else if (sum < 0) {
